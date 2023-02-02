99fbb1734b166b8d8dfbb6d63355ade8b8490885
미니 <cow970814@naver.com>
2023-02-03 00:52:02 +0900

    feat : PlayViewController Control Button State 변경 메서드 추가

--- GyroData/Resources/Controller/PlayViewController.swift
+++ GyroData/Resources/Controller/PlayViewController.swift
@@ -28,13 +28,24 @@ final class PlayViewController: UIViewController {
     }()
     
     private let controlButton: UIButton = {
-        let button = UIButton(configuration: .plain())
+        let button = UIButton()
         
         let configure = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 40), scale: .large)
-        let playImage = UIImage(systemName: "play.fill", withConfiguration: configure)
 
+        let playImage = UIImage(systemName: "play.fill", withConfiguration: configure)
         button.setImage(playImage, for: .normal)
+        
+        let pauseImage = UIImage(systemName: "pause.fill", withConfiguration: configure)
+        button.setImage(pauseImage, for: .selected)
+        
         return button
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.textAlignment = .center
+        return label
     }()
     
     init(viewType: PlayViewController.viewType, metaData: TransitionMetaData) {
@@ -51,6 +62,23 @@ final class PlayViewController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        setButtonActions()
+    }
+}
+
+extension PlayViewController {
+    func setButtonActions() {
+        controlButton.addTarget(
+            self,
+            action: #selector(didTapControlButton),
+            for: .touchUpInside
+        )
+    }
+}
+
+extension PlayViewController {
+    @objc func didTapControlButton() {
+        controlButton.isSelected.toggle()
     }
 }
 
@@ -73,8 +101,13 @@ extension PlayViewController {
         }
         
         if playType == .play {
-            view.addSubview(controlButton)
-            controlButton.translatesAutoresizingMaskIntoConstraints = false
+            [
+                controlButton,
+                timeLabel
+            ].forEach {
+                view.addSubview($0)
+                $0.translatesAutoresizingMaskIntoConstraints = false
+            }
         }
     }
     
@@ -105,13 +138,20 @@ extension PlayViewController {
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             controlButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-            controlButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -120)
+            controlButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -120),
+            timeLabel.centerYAnchor.constraint(equalTo: controlButton.centerYAnchor),
+            timeLabel.leadingAnchor.constraint(equalTo: controlButton.trailingAnchor),
+            timeLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
         ])
     }
     
     func setComponentsValues() {
         dateLabel.text = metaData.saveDate
         viewTypeLabel.text = self.playType.rawValue
+        
+        if playType == .play {
+            timeLabel.text = metaData.recordTime.description
+        }
     }
     
     func setNavigationBar() {
